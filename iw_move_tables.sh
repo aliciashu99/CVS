8dac8703ba57297efc68a353d6a88fdf42490191
alicia <ashu@alicias-MacBook-Pro.local>
2016-10-20 13:07:31 -0700

    Remove hdfs header from source location in create table command if destination location is specified

--- iw_move_tables.sh
+++ iw_move_tables.sh
@@ -2,6 +2,17 @@
 
 usage ()
 {
+  echo ' '
+  echo 'The script will not move any data. Users will run it after the data has been moved to new locations.'
+  echo 'The script will save Hive Create External Table commands for all the tables in a file output.txt.'
+  echo 'Users should view this file and make sure the commands are correct before running the commands.'
+  echo 'Note: if the source directory containing hdfs path and destination directory is specified, '
+  echo 'the script will remove the hdfs header and leave the directory starting with (/).'
+  echo 'For example, if the source directory LOCATION is specified as /foo in hdfs:'
+  echo 'hdfs://ip-172.ec2.internal:8020/foo/bardb/87fc6c5ae4b0d549be24323e/merged/orc'
+  echo 'and the destination is specified as /bar, the new hdfs destination LOCATION becomes:'
+  echo '/bar/bardb/87fc6c5ae4b0d549be24323e/merged/orc'
+  echo ' '
   echo 'Usage : ./iw_move_tables.sh'
   echo '-s|--sourcedb <Hive DB name> '
   echo '-t|--table <Optional: Hive table name. If not specified use ALL> '
@@ -64,16 +75,11 @@ echo SOURCE DIR      = "${SOURCEDIR}"
 echo DESTINATION DIR = "${DESTDIR}"
 echo RUN             = "${RUN}"
 
-if [[ -n $1 ]]; then
-  echo "Last line of file specified as non-opt/last argument:"
-  tail -1 $1
-fi
-
 if [[(-z "$SOURCEDIR") && (-n "$DESTDIR")]]; then
   echo "Did not specify source directory";
   exit 1;
 elif [[(-n "$SOURCEDIR") && (-z "$DESTDIR")]]; then
-  echo "Did not specifiy destination directory, use source directory";
+  echo "Did not specifiy destination directory, Will use source directory";
 fi
 
 if [[(-z "$DESTDB")]]; then
@@ -83,12 +89,12 @@ if [[(-z "$DESTDB")]]; then
 fi
 
 if [[(-z "$TABLE")]]; then
-  echo "Run the script for all the tables";
+  echo "Running the script for all the tables";
   showtbl=$(hive -e "USE $SOURCEDB; SHOW tables");
   echo showtbl = $showtbl;
   echo "${showtbl}" > show.txt;
 else
-  echo "Run the script for single table ${TABLE}";
+  echo "Running the script for single table ${TABLE}";
   showtbl=$TABLE
 fi
 
@@ -100,13 +106,28 @@ do
   sed -i "s/$quote$SOURCEDB.$table/$quote$DESTDB.$table/g" output.txt;
   if [[(-n "$DESTDIR")]]; then
     echo "Replace source dir with destination dir";
-    sed -i "s/$SOURCEDIR/$DESTDIR/g" output.txt;
+    if grep -q "hdfs" output.txt; then
+      ddir=$(grep "hdfs" output.txt)
+      ddir=$(echo $ddir | sed -e 's/^[ \t]*//')
+      pos=$(echo $ddir | grep -aob '://'| grep -oE '[0-9]+')
+      pos=$(($pos+3))
+      ddir=$(echo ${ddir:$pos})
+      pos=$(echo $ddir | grep -aob '/'| grep -oE '[0-9]+')
+      newpos=$(echo $pos | awk '{print $1}')
+      echo ${ddir:$newpos} > tmp.txt
+      sed -i "s%$SOURCEDIR%$DESTDIR%g" tmp.txt
+      sed -i "s/^/'/" tmp.txt
+      substitute=$(cat tmp.txt)
+      sed -i "/hdfs/c $substitute" output.txt
+    else
+      sed -i "s%$SOURCEDIR%$DESTDIR%g" output.txt
+    fi
   fi
   echo ";" >> output.txt
 done
 cat output.txt
 if "$RUN"; then
-  echo "Run generated CREATE EXTENAL TABLE commands in destination database";
+  echo "Running generated CREATE EXTENAL TABLE commands in destination database";
   hive -f output.txt;
 fi
 
